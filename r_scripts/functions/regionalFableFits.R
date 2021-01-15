19ee2a3b98ef3ecb0f6fa37b9789559121d49360
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-01-15 15:59:53 +0000

    Local functions removed

--- r_scripts/functions/regionalFableFits.R
+++
@@ -1,9 +0,0 @@
-regionalFableFits <- function(data, region){
-  # Return various model objects for each region
-  print(region)
-  region <- enquo(region)
-
-  data %>%
-    filter(regional_unit == !!region) %>%
-    fableModels()
-}
