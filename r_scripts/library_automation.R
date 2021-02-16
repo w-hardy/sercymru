f5fc74ba89c9ad692d7fcf59f9fd26105c685a45
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-02-16 10:15:56 +0000

    Copying Mendeley library from local drive to project file

---
+++ r_scripts/library_automation.R
@@ -0,0 +1,5 @@
+# cronR::cron_rstudioaddin()
+
+file.copy("/Users/willhardy/Dropbox/references/library.bib",
+          "/Users/willhardy/OneDrive - Outreach Rescue/covid_prescribing/sercymru/references/",
+          overwrite = TRUE)
