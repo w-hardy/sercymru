4087338e7aae84e63fa26dce14f4ea7ea3447e76
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-08-21 18:48:25 +0100

    Path update

--- r_scripts/library_automation.R
+++ r_scripts/library_automation.R
@@ -1,5 +1,5 @@
 # cronR::cron_rstudioaddin()
 
 file.copy("/Users/willhardy/Dropbox/references/library.bib",
-          "/Users/willhardy/OneDrive - Outreach Rescue/covid_prescribing/sercymru/references/",
+          "/Users/willhardy/OneDrive - Outreach Rescue ORMS/covid_prescribing/sercymru/references/",
           overwrite = TRUE)
